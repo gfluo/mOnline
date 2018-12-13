5a227ed4c1d050764eff5ab157c62118ec786c6d
git <519665744@qq.com>
2018-12-13 20:46:18 +0800

    优化已上线商品接口

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ if (process.argv.length > 2) {
             try {
                 let mList = await platformW.mList(process.argv[3]);
                 console.log(mList.products_info);
+                console.log(`已在线商品数量${mList.length}`);
             } catch (e) {
                 console.error(e);
             }
