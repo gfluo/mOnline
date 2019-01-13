1d7119b690930d566ebf10613fd558de224913a7
git <519665744@qq.com>
2019-01-13 23:57:01 +0800

    打印字符串

--- app.js
+++ app.js
@@ -101,8 +101,8 @@ async function start() {
             let relate = JSON.parse(relateStr);
             let wxCidInfo = relate[cidInfo.top.cid];
             let wxSubInfo = await platformW.getCategorySub(wxCidInfo.wx_cid);
-            console.log(wxSubInfo);
-            wxMOpData.product_base.property = [{id: `关键词`, vid: `${ms[i].keywords}`}]
+            ///console.log(wxSubInfo);
+            ///wxMOpData.product_base.property = [{id: `关键词`, vid: `${ms[i].keywords}`}]
             wxMOpData.product_base.category_id = [];
             wxMOpData.product_base.category_id.push(wxSubInfo.id);
             wxMOpData.product_base.name = ms[i].goods_name;
